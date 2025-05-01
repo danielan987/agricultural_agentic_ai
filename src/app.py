b908cc38bfaf41eaea15d7fc08e09f5c430d7671
Daniel An <118602647+danielan987@users.noreply.github.com>
2025-04-30 21:21:03 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,6 @@ import folium
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import duckduckgo_search 
 from duckduckgo_search import DDGS
 from geopy.geocoders import Nominatim
 import requests
